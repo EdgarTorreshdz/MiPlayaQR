6a430db0cb7543b7fe08c0e698161c27e67eb24b
Edgar <eath2497@gmail.com>
2019-10-05 23:04:57 -0500

    ayuda poner variables en center:

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,36 @@
 
 @extends('layouts.app')
 @section('content')
+    <div id="startLat"></div>
+    <div id="startLon"></div>
     <div id="map"></div>
+
 @endsection
     <script>
-      var map;
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 21.1321181, lng: -86.8563852},
-          zoom: 8
-        });
-      }
+var startPos;
+var lati;
+var long;
+window.onload = function() {
+  
+  var geoSuccess = function(position) {
+    startPos = position;
+    document.getElementById('startLat').innerHTML = startPos.coords.latitude;
+    document.getElementById('startLon').innerHTML = startPos.coords.longitude;
+    lati = startPos.coords.latitude;
+    long = startPos.coords.longitude;
+  };
+  
+
+  navigator.geolocation.getCurrentPosition(geoSuccess);
+};
+
+        var map;
+        function initMap() {
+            map = new google.maps.Map(document.getElementById('map'), {
+                center: {lat: 21.1321181, lng: -86.8563852},
+            zoom: 8
+            });
+        }
     </script>
     <script src="https://maps.googleapis.com/maps/api/js?AIzaSyBw-vOkQq7mPD-46S1GOq-dCPcmFmxNkko&callback=initMap"
     async defer></script>
