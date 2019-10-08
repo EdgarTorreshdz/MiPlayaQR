e1b4d5f94ebdb9d68f154db3cd885591bb8623d3
Edgar <eath2497@gmail.com>
2019-10-08 14:35:04 -0500

    Cambios

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -77,14 +77,13 @@
                             @endif
                             @if(Auth::user()->permiso==2)
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('/playas') }}">{{ __('Playas') }}</a>
+                                <a class="nav-link" href="{{ route('playas') }}">{{ __('Playas') }}</a>
                             </li>
                             @endif
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
-
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
