bf0c0aec266f6e3c5e00136cccc4cc82475e0a5f
Edgar <eath2497@gmail.com>
2019-10-08 14:58:02 -0500

    usuario

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        DB::table('users')->insert([
+            'name' => 'Admin',
+            'email' => 'eath2497@gmail.com',
+            'password' => bcrypt('Uniproyecto_1997'),
+            'permiso' => '1',
+        ]);
     }
 }
