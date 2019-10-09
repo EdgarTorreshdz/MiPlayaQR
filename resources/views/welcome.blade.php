ce818c019702c668bfcf5fa926c8aba7a4bb1fc1
Edgar <eath2497@gmail.com>
2019-10-08 20:17:34 -0500

    la pinshi L

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -44,7 +44,7 @@
         <nav class="navbar navbar-expand-md navbar-light shadow-sm" style="height: auto;">
             <div class="container">
             <a class="navbar-brand" href="{{ url('/') }}">
-                <img src="images/logo.png" alt="" style="height: 80px;">
+                <img src="images/Logo.png" alt="" style="height: 80px;">
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
